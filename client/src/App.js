f4d2467acd63c78a8c3f07cff6e21c5b9fe86685
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2023-03-03 14:52:58 +0530

    SignUp and Logout DOne, Login still pending

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import {  Box } from '@mui/material'
+import DataProvider from './components/context/DataProvider';
+//Wrapping components inside DataProvider so that we can use states inside them
 function App() {
   return (
     <div className="App">
+  
+    <DataProvider>
 <Header/>
 <Box style={{marginTop:54}}>
 <Home/>
 </Box>
+</DataProvider>
     </div>
   );
 }
